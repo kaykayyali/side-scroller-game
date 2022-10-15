55277e229a0a70c15e5cb9fc669ec1f683a7c500
Ahabo <gfons009@gmail.com>
2022-10-15 15:01:15 -0400

    Working on ground

--- public/main.js
+++ public/main.js
@@ -2,12 +2,8 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
+    backgroundColor: '#9adaea',
+
     scene: {
         preload: preload,
         create: create
@@ -19,30 +15,13 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('https://labs.phaser.io');
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('ground', 'assets/Ground.jpg');
+
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+    ground = this.add.image(0, 400, 'ground').setOrigin(0);
+    ground.setScale(1/6);
 }
